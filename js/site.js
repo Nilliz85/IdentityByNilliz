c8ecbd65bd3180bd018535ed7d9391f84a183e29
Per Edward Nilsen <nilliz85@gmail.com>
2023-02-24 12:44:18 +0100

    added js

--- js/site.js
+++ js/site.js
@@ -5,3 +5,13 @@ const header = document.querySelector(".header");
 hamburger.addEventListener("click", () => {
   navList.classList.toggle("show");
 });
+
+const navHeight = header.getBoundingClientRect().height;
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  if (scrollHeight > navHeight) {
+    header.classList.add("fix");
+  } else {
+    header.classList.remove("fix");
+  }
+});
