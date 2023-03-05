f0f498472795369d402e01de3d8252eed5da0bb7
Per Edward Nilsen <nilliz85@gmail.com>
2023-03-05 21:16:21 +0100

    started js

--- js/wp-rest-api.js
+++ js/wp-rest-api.js
@@ -1,6 +1,2 @@
 /**/
 /*-------------------- WP REST API GET Request --------------------*/
-fetch("https://headless-wp.pernilsen.dev/wp-json/wp/v2/posts?per_page=10")
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((error) => console.error(error));
